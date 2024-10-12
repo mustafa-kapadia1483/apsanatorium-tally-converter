f722a029f10aa5dfe88e74ad6b7b62fc80453923
Mustafa Kapadia <mustafakapadia76@gmail.com>
2024-10-12 13:56:05 +0530

    removed filename input from user & generated files will always replace old ones

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Steps to use this tool
 1. Make sure to have node installed, if not present download from [here](https://nodejs.org/en)
 2. Download this code as zip
 3. Extract the files once download is complete
-4. Place the raw file receipt file in the same folder
+4. Place the raw file receipt file in the same folder. Make sure rename it to "q"
 5. Open windows terminal by right clicking and selecting "Open in Terminal" or typing "wt" in windows explorer file path
 6. If opening for the first time then run this comman "npm i" else skip this part
-7. Type the command `node . <filanme>.<extension>`
-8. Tally accouting voucher import file will be created in the same folder with prefix as _Accounting-Voucher-_ & tally ledger import file will be created with prefix _Ledger-_
+7. Type the command `node .`
+8. Tally accouting voucher import file will be created in the same folder as _Accounting-Voucher.xlsx_ & tally ledger import file will be created as _Ledger.xlsx_
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import path from "path";
 import { fileURLToPath } from "url";
-import strftime from "./utils/strftime.js";
 import excelToArray from "./utils/excelToArray.js";
 import getAccountingVoucherData from "./utils/getAccountingVoucherData.js";
 import createXls from "./utils/createXls.js";
@@ -9,7 +8,7 @@ import createXls from "./utils/createXls.js";
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
-const filePath = `${__dirname}/${process.argv.at(-1)}`;
+const filePath = `${__dirname}/q.xls`;
 
 const receiptDataArray = excelToArray(filePath);
 
@@ -19,11 +18,7 @@ const { tallyAccountingVoucherArray, ledgerArray } =
 createXls(
   tallyAccountingVoucherArray,
   "Accounting Voucher",
-  `Accounting-Voucher-${strftime("%Y-%m-%d-%H-%M-%S")}.xlsx`
+  "Accounting-Voucher.xlsx"
 );
 
-createXls(
-  ledgerArray,
-  "Ledger",
-  `Ledger-${strftime("%Y-%m-%d-%H-%M-%S")}.xlsx`
-);
+createXls(ledgerArray, "Ledger", "Ledger.xlsx");
